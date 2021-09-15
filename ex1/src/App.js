89edcf900b5db311c57da9d9ce8b70b5d4f5c90d
satheesh <sekarsatheesh30@gmail.com>
2021-09-16 00:06:49 +0530

    initialcommit

---
+++ ex1/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Clock from './components/Clock'
+import Timer from './components/Timer'
+
+function App() {
+  return (
+    <div className="App">
+      <Timer></Timer>
+    </div>
+  );
+}
+
+export default App;
+
+
